24d06645fb6d657484bd56bf661e98b77a4e5d36
Shin <shinjpn1@gmail.com>
2026-02-12 20:26:56 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqloader',
-    version='0.2.3',
+    version='0.2.4',
     description='py_sqloader package',
     author='horrible-gh',
     author_email='shinjpn1@gmail.com',
